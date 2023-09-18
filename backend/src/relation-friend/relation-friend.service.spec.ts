e0be4fabefbabb1e3c1a68a687c43f929a06b5e7
Isioblast <th.dv74@gmail.com>
2023-09-18 13:37:44 +0200

    Tests need to be implemented

--- backend/src/relation-friend/relation-friend.service.spec.ts
+++ backend/src/relation-friend/relation-friend.service.spec.ts
@@ -2,17 +2,40 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { RelationFriendService } from './relation-friend.service'
 
 describe('RelationFriendService', () => {
-  let service: RelationFriendService
+  let relationFriendService: RelationFriendService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [RelationFriendService]
     }).compile()
 
-    service = module.get<RelationFriendService>(RelationFriendService)
+    relationFriendService = module.get<RelationFriendService>(
+      RelationFriendService
+    )
   })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined()
+  it('create a new relaton in DB', () => {
+    const creatRet = relationFriendService.create('defined', 'defined')
+    expect(creatRet).toBeDefined()
+  })
+
+  it("return all friend of a user's id", () => {
+    const friends = relationFriendService.findAll('defined')
+    expect(friends).toBeDefined()
+  })
+
+  it('verify if two users in WRONG order are friend', () => {
+    relationFriendService
+    expect(relationFriendService).toBeDefined()
+  })
+
+  it('verify if two users in RIGHT order are friend', () => {
+    const isFriendRet = relationFriendService.isFriend('defined', 'definde')
+    expect(isFriendRet).toBeDefined()
+  })
+
+  it("delete a relation by it's id", () => {
+    const deletedRel = relationFriendService.deleteById('defined', 'defined')
+    expect(deletedRel).toBeDefined()
   })
 })
