94b81b78f3fa0c7ec851dd4ae078e9400446540c
Robin <Geral.robin@yahoo.fr>
2023-09-22 00:03:15 +0200

    channel mutation seems ok

--- backend/src/channel/channel.service.ts
+++ backend/src/channel/channel.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma/prisma.service'
+import { Prisma, Channel, EGameType } from '@prisma/client'
 
 @Injectable()
-export class ChannelService {}
+export class ChannelService {
+  constructor(private prisma: PrismaService) {}
+
+  //**************************************************//
+  //  MUTATION
+  //**************************************************//
+
+  async create(data: Prisma.ChannelCreateInput): Promise<Channel> {
+    return this.prisma.channel.create({
+      data
+    })
+  }
+
+  async update(id: string, data: Prisma.ChannelUpdateInput): Promise<Channel> {
+    return this.prisma.channel.update({
+      where: {
+        id
+      },
+      data
+    })
+  }
+
+  async delete(id: string): Promise<Channel> {
+    return this.prisma.channel.delete({
+      where: {
+        id
+      }
+    })
+  }
+}
