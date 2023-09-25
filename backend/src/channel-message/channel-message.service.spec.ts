9a6852ccf8002607135143f9a620664715581233
Isioblast <th.dv74@gmail.com>
2023-09-25 18:31:47 +0200

    wip

--- backend/src/channel-message/channel-message.service.spec.ts
+++ backend/src/channel-message/channel-message.service.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { ChannelMessageService } from './channel-message.service'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+//                                                         Table "public.ChannelMessage"
+//   Column   |              Type              | Collation | Nullable |      Default      | Storage  | Compression | Stats target | Description
+// -----------+--------------------------------+-----------+----------+-------------------+----------+-------------+--------------+-------------
+//  id        | text                           |           | not null |                   | extended |             |              |
+//  senderId  | text                           |           | not null |                   | extended |             |              |
+//  channelId | text                           |           | not null |                   | extended |             |              |
+//  content   | text                           |           | not null |                   | extended |             |              |
+//  createdAt | timestamp(3) without time zone |           | not null | CURRENT_TIMESTAMP | plain    |             |              |
+// Indexes:
+//     "ChannelMessage_pkey" PRIMARY KEY, btree (id)
+// Foreign-key constraints:
+//     "ChannelMessage_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"(id) ON UPDATE CASCADE ON DELETE CASCADE
+//     "ChannelMessage_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"(id) ON UPDATE CASCADE ON DELETE RESTRICT
 
 describe('ChannelMessageService', () => {
-  let service: ChannelMessageService
+  let channelMessageService: ChannelMessageService
+  let prismaService: PrismaService
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChannelMessageService]
+      providers: [ChannelMessageService, PrismaService]
     }).compile()
 
-    service = module.get<ChannelMessageService>(ChannelMessageService)
+    channelMessageService = module.get<ChannelMessageService>(
+      ChannelMessageService
+    )
+    prismaService = module.get<PrismaService>(PrismaService)
   })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined()
+  afterAll(async () => {
+    prismaService.$executeRaw`DELETE FROM "public"."ChannelMessage"`
+    prismaService.$executeRaw`DELETE FROM "public"."Channel"`
+    prismaService.$executeRaw`DELETE FROM "public"."User"`
+    await prismaService.$disconnect()
+  })
+
+  describe('Test channel-message Mutation', () => {
+    it('should be defined', async () => {
+      expect(channelMessageService).toBeDefined()
+    })
+  })
+
+  describe('Test Query', () => {
+    it('should be defined', async () => {
+      expect(channelMessageService).toBeDefined()
+    })
+  })
+
+  describe('Test Error', () => {
+    it('should be defined', async () => {
+      expect(channelMessageService).toBeDefined()
+    })
   })
 })
--- backend/src/channel-message/channel-message.service.ts
+++ backend/src/channel-message/channel-message.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common'
+import { ChannelMessage } from '@prisma/client'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class ChannelMessageService {}
+export class ChannelMessageService {
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(id: string): Promise<ChannelMessage | null> {
+    return this.prisma.channelMessage.findUnique({
+      where: {
+        id
+      }
+    })
+  }
+
+  //create
+  //findAllFromChannel
+  //update
+  //delete
+}
