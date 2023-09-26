4b782059bfb98ffef1b4cd519f6e19ded27c9f79
Isioblast <th.dv74@gmail.com>
2023-09-26 16:09:34 +0200

    Implementing channel-service.ts

--- backend/src/channel-message/channel-message.service.ts
+++ backend/src/channel-message/channel-message.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common'
-import { ChannelMessage } from '@prisma/client'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { ChannelMessage, Prisma } from '@prisma/client'
+import { PrismaService } from '../prisma/prisma.service'
+import { ExceptionTryingToUpdateID } from '../user/exceptions/channel-message.exception'
 
 @Injectable()
 export class ChannelMessageService {
   constructor(private prisma: PrismaService) {}
+
+  async create(
+    data: Prisma.ChannelMessageCreateInput
+  ): Promise<ChannelMessage> {
+    return this.prisma.channelMessage.create({
+      data
+    })
+  }
 
   async findOne(id: string): Promise<ChannelMessage | null> {
     return this.prisma.channelMessage.findUnique({
@@ -14,8 +23,40 @@ export class ChannelMessageService {
     })
   }
 
-  //create
-  //findAllFromChannel
-  //update
-  //delete
+  async findAllFromChannel(channelId: string) {
+    return this.prisma.channelMessage.findMany({
+      where: {
+        channelId
+      }
+    })
+  }
+
+  async findAllFromChannelIdsAndUserId(channelId: string, senderId: string) {
+    return this.prisma.channelMessage.findMany({
+      where: {
+        AND: [{ channelId, senderId }]
+      }
+    })
+  }
+
+  async update(
+    id: string,
+    data: Prisma.ChannelMessageUpdateInput
+  ): Promise<ChannelMessage> {
+    if (data.id) throw new ExceptionTryingToUpdateID()
+    return this.prisma.channelMessage.update({
+      where: {
+        id
+      },
+      data
+    })
+  }
+
+  async delete(id: string): Promise<ChannelMessage> {
+    return this.prisma.channelMessage.delete({
+      where: {
+        id
+      }
+    })
+  }
 }
