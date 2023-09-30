15d14072090f8b0e899da074cbf88190da964e99
Isioblast <th.dv74@gmail.com>
2023-09-30 20:11:35 +0200

    Added expections for new check in the ChannelMessage service

--- backend/src/user/exceptions/channel-message.exception.ts
+++ backend/src/user/exceptions/channel-message.exception.ts
@@ -1,9 +1,32 @@
 import { HttpException, HttpStatus } from '@nestjs/common'
 
+export class ExceptionEmptyMessage extends HttpException {
+  constructor() {
+    super('The message content is empty', HttpStatus.CONFLICT)
+  }
+}
+
 export class ExceptionTryingToUpdateID extends HttpException {
   constructor() {
     super(
       'You are trying to change the id of a channel message',
+      HttpStatus.CONFLICT
+    )
+  }
+}
+
+export class ExceptionTryingToUpdateCreationDate extends HttpException {
+  constructor() {
+    super(
+      'You are trying to change the creation date of a channel message',
+      HttpStatus.CONFLICT
+    )
+  }
+}
+export class ExceptionTryingToUpdateSenderID extends HttpException {
+  constructor() {
+    super(
+      "You are trying to change the senders's id of a channel message",
       HttpStatus.CONFLICT
     )
   }
