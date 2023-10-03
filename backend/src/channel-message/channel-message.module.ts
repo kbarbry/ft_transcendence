516cffde19b287a585a21619fc88e275248e3efe
kbarbry <kellianbarbry@gmail.com>
2023-10-03 13:32:15 +0200

    added prisma services in all modules

--- backend/src/channel-message/channel-message.module.ts
+++ backend/src/channel-message/channel-message.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ChannelMessageService } from './channel-message.service'
+import { PrismaModule } from 'src/prisma/prisma.module'
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   exports: [ChannelMessageService],
   providers: [ChannelMessageService]
 })
--- backend/src/channel/channel.module.ts
+++ backend/src/channel/channel.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ChannelService } from './channel.service'
+import { PrismaModule } from 'src/prisma/prisma.module'
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   exports: [ChannelService],
   providers: [ChannelService]
 })
