8b8948cbb28ea36f4272b258ec1e7260ed05176e
Robin <Geral.robin@yahoo.fr>
2023-10-10 18:26:40 +0200

    First DTO done

--- backend/src/user/dto/create-user.input.ts
+++ backend/src/user/dto/create-user.input.ts
@@ -1,7 +1,37 @@
-import { InputType, Int, Field } from '@nestjs/graphql'
+import { InputType, Field, Float } from '@nestjs/graphql'
+import { EStatus } from '@prisma/client'
+import {
+  IsEmail,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MaxLength
+} from 'class-validator'
 
 @InputType()
 export class CreateUserInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number
+  @Field(() => String)
+  @IsOptional()
+  @IsUrl()
+  @MaxLength(2083)
+  avatarUrl?: string
+
+  @Field(() => String)
+  @IsEmail()
+  mail: string
+
+  @Field(() => String)
+  @IsString({ always: true })
+  @MaxLength(30)
+  username: string
+
+  @Field(() => EStatus)
+  @IsEnum(EStatus)
+  status: EStatus
+
+  @Field(() => Float)
+  @IsNumber({ allowInfinity: false, allowNaN: false })
+  level: number
 }
