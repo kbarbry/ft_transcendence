f2d0f965f1b793599103a35f775c0b5efb58a359
Isioblast <th.dv74@gmail.com>
2023-10-11 19:17:23 +0200

    PrivateMessage: removed unused exceptions

--- backend/src/channel/exceptions/private-message.exception.ts
+++ backend/src/channel/exceptions/private-message.exception.ts
@@ -1,28 +1,7 @@
 import { HttpException, HttpStatus } from '@nestjs/common'
 
-export class ExceptionTryingToUpdatePrivateMessageID extends HttpException {
-  constructor() {
-    super(
-      'You are trying to change the id of a PrivateMessage',
-      HttpStatus.CONFLICT
-    )
-  }
-}
-
-export class ExceptionTryingToUpdateUsersId extends HttpException {
-  constructor() {
-    super('You are trying to change the id of the users', HttpStatus.CONFLICT)
-  }
-}
-
 export class ExceptionPrivateMessageYourself extends HttpException {
   constructor() {
     super('You are trying to send a message to yourself', HttpStatus.CONFLICT)
-  }
-}
-
-export class ExceptionTryingToUpdateDateMessage extends HttpException {
-  constructor() {
-    super('You are trying to update the date of a message', HttpStatus.CONFLICT)
   }
 }
