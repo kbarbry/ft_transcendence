95cf08c4dbb96f3e5240c156617e2b3064b1a983
cben-bar <ana.kronik@hotmail.fr>
2023-10-15 21:00:33 +0200

    relation-blocked.resolver

---
+++ backend/src/relation-blocked/relation-blocked.resolver.ts
@@ -0,0 +1,51 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql'
+import { RelationBlockedService } from './relation-blocked.service'
+import { RelationBlocked } from './entities/relation-blocked.entity'
+import { RelationBlockedInput } from './dto/create-relation-blocked.input'
+import { ValidationPipe } from '@nestjs/common'
+
+@Resolver(() => RelationBlocked)
+export class RelationBlockedResolver {
+  constructor(
+    private readonly relationBlockedService: RelationBlockedService
+  ) {}
+
+  //**************************************************//
+  //  MUTATION
+  //**************************************************//
+
+  @Mutation(() => RelationBlocked)
+  async createBlocked(
+    @Args('data', { type: () => RelationBlockedInput }, ValidationPipe)
+    createRelationBlockedDto: RelationBlockedInput
+  ): Promise<RelationBlocked | null> {
+    return this.relationBlockedService.create(createRelationBlockedDto)
+  }
+
+  @Mutation(() => RelationBlocked)
+  async deleteRelationBlocked(
+    @Args('userAId', { type: () => String }) userAId: string,
+    @Args('userBId', { type: () => String }) userBId: string
+  ) {
+    return this.relationBlockedService.delete(userAId, userBId)
+  }
+
+  //**************************************************//
+  //  QUERY
+  //**************************************************//
+
+  @Query(() => Boolean)
+  async isBlocked(
+    @Args('userAId', { type: () => String }) userAId: string,
+    @Args('userBId', { type: () => String }) userBId: string
+  ): Promise<boolean> {
+    return this.relationBlockedService.isBlocked(userAId, userBId)
+  }
+
+  @Query(() => RelationBlocked)
+  findAllBlockedByUser(
+    @Args('id', { type: () => String }) id: string
+  ): Promise<string[]> {
+    return this.relationBlockedService.findAllBlockedByUser(id)
+  }
+}
