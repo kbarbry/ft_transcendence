eacbc8478df58c867991e7bcb8b5084cb1363730
Isioblast <th.dv74@gmail.com>
2023-10-16 14:56:03 +0200

    ChannelInvited: Add resolver

--- backend/src/channel-invited/channel-invited.module.ts
+++ backend/src/channel-invited/channel-invited.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ChannelInvitedService } from './channel-invited.service'
 import { PrismaModule } from 'src/prisma/prisma.module'
+import { ChannelInvitedResolver } from './channel-invited.resolver';
 
 @Module({
   imports: [PrismaModule],
   exports: [ChannelInvitedService],
-  providers: [ChannelInvitedService]
+  providers: [ChannelInvitedService, ChannelInvitedResolver]
 })
 export class ChannelInvitedModule {}
---
+++ backend/src/channel-invited/channel-invited.resolver.ts
@@ -0,0 +1,52 @@
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql'
+import { ChannelInvited } from './entities/channel-invited.entity'
+import { ChannelInvitedService } from './channel-invited.service'
+import { CreateChannelInvitedInput } from './dto/create-channel-invited.input'
+import { ValidationPipe } from '@nestjs/common'
+
+@Resolver(() => ChannelInvited)
+export class ChannelInvitedResolver {
+  constructor(private readonly channelInvitedServie: ChannelInvitedService) {}
+
+  //**************************************************//
+  //  MUTATION
+  //**************************************************//
+  @Mutation(() => ChannelInvited)
+  async createChannelInvited(
+    @Args('data', { type: () => CreateChannelInvitedInput }, ValidationPipe)
+    createChannelInvitedDto: CreateChannelInvitedInput
+  ): Promise<ChannelInvited> {
+    return this.channelInvitedServie.create(createChannelInvitedDto)
+  }
+
+  @Mutation(() => ChannelInvited)
+  async deleteChannelInvited(
+    @Args('userId', { type: () => String }, ValidationPipe)
+    userId: string,
+    @Args('channelId', { type: () => String }, ValidationPipe)
+    channelId: string
+  ): Promise<ChannelInvited> {
+    return this.channelInvitedServie.delete(userId, channelId)
+  }
+
+  //**************************************************//
+  //  QUERY
+  //**************************************************//
+  @Query(() => ChannelInvited)
+  async findOne(
+    @Args('userId', { type: () => String }, ValidationPipe)
+    userId: string,
+    @Args('channelId', { type: () => String }, ValidationPipe)
+    channelId: string
+  ): Promise<ChannelInvited | null> {
+    return this.channelInvitedServie.findOne(userId, channelId)
+  }
+
+  @Query(() => [ChannelInvited])
+  async findAllInChannel(
+    @Args('channelId', { type: () => String }, ValidationPipe)
+    channelId: string
+  ): Promise<ChannelInvited[]> {
+    return this.channelInvitedServie.findAllInChannel(channelId)
+  }
+}
