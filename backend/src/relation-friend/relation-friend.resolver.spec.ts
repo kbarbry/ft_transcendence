c2c98dcc80185e7d62b7348ad10059236f45ba18
rgeral <geral.robin@yahoo.fr>
2023-10-20 14:37:02 +0200

    RelationFriend Resolver spec done for DTO

--- backend/src/relation-friend/relation-friend.resolver.spec.ts
+++ backend/src/relation-friend/relation-friend.resolver.spec.ts
@@ -1,20 +1,252 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { RelationFriendResolver } from './relation-friend.resolver'
 import { RelationFriendService } from './relation-friend.service'
+import { ArgumentMetadata, ValidationPipe } from '@nestjs/common'
+import { RelationFriendInput } from './dto/create-relation-friend.input'
+import { PrismaService } from '../prisma/prisma.service'
 
 describe('UserResolver', () => {
-  let resolver: RelationFriendResolver
+  let relationFriendResolver: RelationFriendResolver
+  const validationPipe = new ValidationPipe()
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RelationFriendResolver, RelationFriendService]
+      providers: [RelationFriendResolver, RelationFriendService, PrismaService]
     }).compile()
 
-    resolver = module.get<RelationFriendResolver>(RelationFriendResolver)
+    relationFriendResolver = module.get<RelationFriendResolver>(
+      RelationFriendResolver
+    )
   })
 
   it('should be defined', () => {
-    expect(resolver).toBeDefined()
+    expect(relationFriendResolver).toBeDefined()
+  })
+  describe('Test Error', () => {
+    describe('userAId - nanoid tests (mandatory)', () => {
+      it('invalid nanoid - empty id', async () => {
+        const data = {
+          userBId: 'ValidIdToTestUserAID1'
+        }
+
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: RelationFriendInput,
+          data: ''
+        }
+        const res = {
+          message: [
+            'userAId must be exactly 21 characters long.',
+            'Invalid nanoid characters.'
+          ],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+      it('invalid nanoid - null nanoId', async () => {
+        const data = {
+          userBId: 'ValidIdToTestUserAID1',
+          userAId: null
+        }
+
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: RelationFriendInput,
+          data: ''
+        }
+        const res = {
+          message: [
+            'userAId must be exactly 21 characters long.',
+            'Invalid nanoid characters.'
+          ],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+      it('too short nanoid', async () => {
+        const data = {
+          userBId: 'ValidIdToTestUserAID1',
+          userAId: 'drfOayPww2tDrePkqqqJ'
+        }
+
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: RelationFriendInput,
+          data: ''
+        }
+        const res = {
+          message: ['userAId must be exactly 21 characters long.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+      it('too long nanoid', async () => {
+        const data = {
+          userBId: 'ValidIdToTestUserAID1',
+          userAId: 'drfOayPww2qqtDrePkqqqwJ'
+        }
+
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: RelationFriendInput,
+          data: ''
+        }
+        const res = {
+          message: ['userAId must be exactly 21 characters long.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+      it('invalid char in nanoid', async () => {
+        const data = {
+          userBId: 'ValidIdToTestUserAID1',
+          userAId: 'drfOayPww 2tDrePkqqqJ'
+        }
+
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: RelationFriendInput,
+          data: ''
+        }
+        const res = {
+          message: ['Invalid nanoid characters.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+    })
+    describe('userBId - nanoid tests (mandatory)', () => {
+      it('invalid nanoid - empty id', async () => {
+        const data = {
+          userAId: 'ValidIdToTestUserBID1'
+        }
+
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: RelationFriendInput,
+          data: ''
+        }
+        const res = {
+          message: [
+            'userBId must be exactly 21 characters long.',
+            'Invalid nanoid characters.'
+          ],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+      it('invalid nanoid - null nanoId', async () => {
+        const data = {
+          userAId: 'ValidIdToTestUserBID1',
+          userBId: null
+        }
+
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: RelationFriendInput,
+          data: ''
+        }
+        const res = {
+          message: [
+            'userBId must be exactly 21 characters long.',
+            'Invalid nanoid characters.'
+          ],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+      it('too short nanoid', async () => {
+        const data = {
+          userAId: 'ValidIdToTestUserBID1',
+          userBId: 'drfOayPww2tDrePkqqqJ'
+        }
+
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: RelationFriendInput,
+          data: ''
+        }
+        const res = {
+          message: ['userBId must be exactly 21 characters long.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+      it('too long nanoid', async () => {
+        const data = {
+          userAId: 'ValidIdToTestUserBID1',
+          userBId: 'drfOayPww2qqtDrePkqqqwJ'
+        }
+
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: RelationFriendInput,
+          data: ''
+        }
+        const res = {
+          message: ['userBId must be exactly 21 characters long.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+      it('invalid char in nanoid', async () => {
+        const data = {
+          userAId: 'ValidIdToTestUserBID1',
+          userBId: 'drfOayPww 2tDrePkqqqJ'
+        }
+
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: RelationFriendInput,
+          data: ''
+        }
+        const res = {
+          message: ['Invalid nanoid characters.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+    })
   })
 })
 /*
