f5f136b2fd76776e2e9e99bad1374b704525be18
Isioblast <th.dv74@gmail.com>
2023-10-20 17:09:33 +0200

    ChannelMessage resolver spec is implemented to test the validation for input

--- backend/src/channel-message/channel-message.resolver.spec.ts
+++ backend/src/channel-message/channel-message.resolver.spec.ts
@@ -1,13 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { ChannelMessageResolver } from './channel-message.resolver'
 import { ChannelMessageService } from './channel-message.service'
+import { ArgumentMetadata, ValidationPipe } from '@nestjs/common'
+import { CreateChannelMessageInput } from './dto/create-channel-message.input'
+import { PrismaService } from '../prisma/prisma.service'
 
 describe('UserResolver', () => {
   let resolver: ChannelMessageResolver
+  const validationPipe = new ValidationPipe()
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChannelMessageResolver, ChannelMessageService]
+      providers: [ChannelMessageResolver, ChannelMessageService, PrismaService]
     }).compile()
 
     resolver = module.get<ChannelMessageResolver>(ChannelMessageResolver)
@@ -15,5 +19,327 @@ describe('UserResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined()
+  })
+
+  describe('Validation Pipe Good', () => {
+    it('Good data', async () => {
+      const data = {
+        content: 'Simple message',
+        senderId: '765ayPlUh0qtDrePkJ87t',
+        channelId: 'pihayPlUh0qtDrePkJ87t'
+      }
+      const metadata: ArgumentMetadata = {
+        type: 'body',
+        metatype: CreateChannelMessageInput,
+        data: ''
+      }
+      const response = await validationPipe.transform(data, metadata)
+      expect(response).toStrictEqual(data)
+    })
+  })
+
+  describe('Test Error', () => {
+    it('Bad content - empty', async () => {
+      const data = {
+        content: '',
+        senderId: '765ayPlUh0qtDrePkJ87t',
+        channelId: 'pihayPlUh0qtDrePkJ87t'
+      }
+      const metadata: ArgumentMetadata = {
+        type: 'body',
+        metatype: CreateChannelMessageInput,
+        data: ''
+      }
+      const res = {
+        message: ['content must be between 1 and 2000 characters long.'],
+        error: 'Bad Request',
+        statusCode: 400
+      }
+      const thrownError = await validationPipe
+        .transform(data, metadata)
+        .catch((error) => error)
+      expect(thrownError.getResponse()).toStrictEqual(res)
+    })
+
+    it('Bad content - too long', async () => {
+      const data = {
+        content:
+          'long message HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+        senderId: '765ayPlUh0qtDrePkJ87t',
+        channelId: 'pihayPlUh0qtDrePkJ87t'
+      }
+      const metadata: ArgumentMetadata = {
+        type: 'body',
+        metatype: CreateChannelMessageInput,
+        data: ''
+      }
+      const res = {
+        message: ['content must be between 1 and 2000 characters long.'],
+        error: 'Bad Request',
+        statusCode: 400
+      }
+      const thrownError = await validationPipe
+        .transform(data, metadata)
+        .catch((error) => error)
+      expect(thrownError.getResponse()).toStrictEqual(res)
+    })
+
+    it('Bad content - undefined', async () => {
+      const data = {
+        senderId: '765ayPlUh0qtDrePkJ87t',
+        channelId: 'pihayPlUh0qtDrePkJ87t'
+      }
+      const metadata: ArgumentMetadata = {
+        type: 'body',
+        metatype: CreateChannelMessageInput,
+        data: ''
+      }
+      const res = {
+        message: [
+          'content must be between 1 and 2000 characters long.',
+          'content must be a string.'
+        ],
+        error: 'Bad Request',
+        statusCode: 400
+      }
+      const thrownError = await validationPipe
+        .transform(data, metadata)
+        .catch((error) => error)
+      expect(thrownError.getResponse()).toStrictEqual(res)
+    })
+
+    describe('Bad SenderId', () => {
+      it('Bad SenderId - bad characters', async () => {
+        const data = {
+          content: 'Simple message',
+          senderId: '765ayPlUh0qtDrePkJ87;',
+          channelId: 'pihayPlUh0qtDrePkJ87t'
+        }
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: CreateChannelMessageInput,
+          data: ''
+        }
+        const res = {
+          message: ['Invalid nanoid characters.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+
+      it('Bad SenderId - too short', async () => {
+        const data = {
+          content: 'Simple message',
+          senderId: '765ayPlUh0qtDrePkJ87',
+          channelId: 'pihayPlUh0qtDrePkJ87t'
+        }
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: CreateChannelMessageInput,
+          data: ''
+        }
+        const res = {
+          message: ['senderId must be exactly 21 characters long.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+
+      it('Bad SenderId - too long', async () => {
+        const data = {
+          content: 'Simple message',
+          senderId: '765ayPlUh0qtDrePkJ87tt',
+          channelId: 'pihayPlUh0qtDrePkJ87t'
+        }
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: CreateChannelMessageInput,
+          data: ''
+        }
+        const res = {
+          message: ['senderId must be exactly 21 characters long.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+
+      it('Bad SenderId - wrong type', async () => {
+        const data = {
+          content: 'Simple message',
+          senderId: 75,
+          channelId: 'pihayPlUh0qtDrePkJ87t'
+        }
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: CreateChannelMessageInput,
+          data: ''
+        }
+        const res = {
+          message: [
+            'senderId must be exactly 21 characters long.',
+            'Invalid nanoid characters.'
+          ],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+
+      it('Bad SenderId - undefined', async () => {
+        const data = {
+          content: 'Simple message',
+          channelId: 'pihayPlUh0qtDrePkJ87t'
+        }
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: CreateChannelMessageInput,
+          data: ''
+        }
+        const res = {
+          message: [
+            'senderId must be exactly 21 characters long.',
+            'Invalid nanoid characters.'
+          ],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+    })
+
+    describe('Bad ChannelId', () => {
+      it('Bad ChannelId - bad characters', async () => {
+        const data = {
+          content: 'Simple message',
+          channelId: '765ayPlUh0qtDrePkJ87;',
+          senderId: 'pihayPlUh0qtDrePkJ87t'
+        }
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: CreateChannelMessageInput,
+          data: ''
+        }
+        const res = {
+          message: ['Invalid nanoid characters.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+
+      it('Bad ChannelId - too short', async () => {
+        const data = {
+          content: 'Simple message',
+          channelId: '765ayPlUh0qtDrePkJ87',
+          senderId: 'pihayPlUh0qtDrePkJ87t'
+        }
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: CreateChannelMessageInput,
+          data: ''
+        }
+        const res = {
+          message: ['channelId must be exactly 21 characters long.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+
+      it('Bad ChannelId - too long', async () => {
+        const data = {
+          content: 'Simple message',
+          channelId: '765ayPlUh0qtDrePkJ87tt',
+          senderId: 'pihayPlUh0qtDrePkJ87t'
+        }
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: CreateChannelMessageInput,
+          data: ''
+        }
+        const res = {
+          message: ['channelId must be exactly 21 characters long.'],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+
+      it('Bad ChannelId - wrong type', async () => {
+        const data = {
+          content: 'Simple message',
+          channelId: 75,
+          senderId: 'pihayPlUh0qtDrePkJ87t'
+        }
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: CreateChannelMessageInput,
+          data: ''
+        }
+        const res = {
+          message: [
+            'channelId must be exactly 21 characters long.',
+            'Invalid nanoid characters.'
+          ],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+
+      it('Bad ChannelId - undefined', async () => {
+        const data = {
+          content: 'Simple message',
+          senderId: 'pihayPlUh0qtDrePkJ87t'
+        }
+        const metadata: ArgumentMetadata = {
+          type: 'body',
+          metatype: CreateChannelMessageInput,
+          data: ''
+        }
+        const res = {
+          message: [
+            'channelId must be exactly 21 characters long.',
+            'Invalid nanoid characters.'
+          ],
+          error: 'Bad Request',
+          statusCode: 400
+        }
+        const thrownError = await validationPipe
+          .transform(data, metadata)
+          .catch((error) => error)
+        expect(thrownError.getResponse()).toStrictEqual(res)
+      })
+    })
   })
 })
