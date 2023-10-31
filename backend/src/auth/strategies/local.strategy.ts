38af6739b1a8400a4eb1610e71b8524ca6b8dd3b
cben-bar <ana.kronik@hotmail.fr>
2023-10-31 19:21:22 +0100

    local authentification

---
+++ backend/src/auth/strategies/local.strategy.ts
@@ -0,0 +1,24 @@
+import { PassportStrategy } from '@nestjs/passport'
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common'
+import { AuthService } from '../auth.service'
+import { Strategy } from 'passport-local'
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({ usernameField: 'email' })
+  }
+  @Inject(AuthService)
+  private readonly authService: AuthService
+
+  async validate(email: string, password: string, callback: CallableFunction) {
+    console.log('INFORMATION USER')
+    console.log(email)
+    console.log(password)
+    const user = await this.authService.validateLocalUser(email, password)
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+    return callback(null, user)
+  }
+}
