21b1c45fe8b618ec4ed941f18b2db7b1c43167d1
Isioblast <th.dv74@gmail.com>
2023-10-31 17:29:13 +0100

    OAuth GitHub Guard and Strategy : WIP

---
+++ backend/src/auth/strategies/github.strategy.ts
@@ -0,0 +1,18 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { PassportStrategy } from '@nestjs/passport'
+import { Profile, Strategy } from 'passport-github2'
+import { AuthService } from '../auth.service'
+
+@Injectable()
+export class GithubOauthStrategy extends PassportStrategy(Strategy, 'github') {
+  constructor() {
+    super() //TODO
+  }
+
+  @Inject(AuthService)
+  private readonly authService: AuthService
+
+  async validate(accessToken: string, _refreshToken: string, profile: Profile) {
+    // TODO
+  }
+}
