6719f9e72789ae979a0cf31b0176dec409a9c2a0
Robin <Geral.robin@yahoo.fr>
2023-11-01 13:27:55 +0100

    guardchanged

--- backend/src/auth/guards/42.guard.ts
+++ backend/src/auth/guards/42.guard.ts
@@ -2,8 +2,4 @@ import { Injectable, ExecutionContext } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
-export class FortyTwoAuthGuard extends AuthGuard('42') {
-  canActivate(context: ExecutionContext) {
-    return super.canActivate(context)
-  }
-}
+export class FortyTwoAuthGuard extends AuthGuard('42') {}
