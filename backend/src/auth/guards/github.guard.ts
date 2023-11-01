ae6422ad13338aec02dc5849a98909a749f26c7d
Isioblast <th.dv74@gmail.com>
2023-11-01 15:02:24 +0100

    GitHub OAuth: Add GitHub guard

--- backend/src/auth/guards/github.guard.ts
+++ backend/src/auth/guards/github.guard.ts
@@ -1,13 +1,5 @@
-import { ExecutionContext, Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
-export class GithubOauthGuard extends AuthGuard('github') {
-  //   async canActivate(context: ExecutionContext) {
-  //     console.log('=== GithubOauthGuard ===') //TODO debug
-  //     const activate = (await super.canActivate(context)) as boolean
-  //     const request = context.switchToHttp().getRequest()
-  //     await super.logIn(request)
-  //     return activate
-  //   }
-}
+export class GithubGuard extends AuthGuard('github') {}
