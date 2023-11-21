5deb9a6106dcff5f3f01d2c4b928530c606a1286
rgeral <geral.robin@yahoo.fr>
2023-11-21 22:53:29 +0100

    fix google guard

--- backend/src/auth/guards/google.guard.ts
+++ backend/src/auth/guards/google.guard.ts
@@ -4,19 +4,9 @@ import { AuthGuard } from '@nestjs/passport'
 @Injectable()
 export class GoogleAuthGuard extends AuthGuard('google') {
   async canActivate(context: ExecutionContext) {
-    try {
-      console.log('Before google GUARD');
       const activate = (await super.canActivate(context)) as boolean;
-      console.log('After google GUARD');
-
       const request = context.switchToHttp().getRequest();
       await super.logIn(request);
-
       return activate;
-    } catch (error) {
-      console.error('Error in GoogleAuthGuard', error);
-      throw error; // Rethrow the error
-    }
   }
 }
-
