39a9d3cffa314f19998e857014cd710904ca21a6
Kellian Barbry <86127065+kbarbry@users.noreply.github.com>
2023-11-23 23:34:28 +0100

    Update github.guard.ts

--- backend/src/auth/guards/github.guard.ts
+++ backend/src/auth/guards/github.guard.ts
@@ -6,7 +6,6 @@ export class GithubGuard extends AuthGuard('github') {
   async canActivate(context: ExecutionContext) {
     const activate = (await super.canActivate(context)) as boolean
     const request = context.switchToHttp().getRequest()
-    console.log('Github guard called')
     await super.logIn(request)
     return activate
   }
