a3c6a9a664e905965723804b0622495260bae8f5
Isioblast <th.dv74@gmail.com>
2023-12-10 20:50:24 +0100

    Pong module update

--- backend/src/pong-game/pong-game.module.ts
+++ backend/src/pong-game/pong-game.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { PongGameResolver } from './pong-game.resolver'
 import { PongGameService } from './pong-game.service'
+import { GameStatService } from 'src/game-stat/game-stat.service'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Module({
-  providers: [PongGameResolver, PongGameService]
+  providers: [PongGameResolver, PongGameService, GameStatService, PrismaService]
 })
 export class PongGameModule {}
