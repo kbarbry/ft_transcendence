619176ab3db6be1968ecd71b28e81a04a62b2264
Isioblast <th.dv74@gmail.com>
2023-12-10 02:43:05 +0100

    update error handling of pong data subscription

--- frontend/src/components/game/subPong.tsx
+++ frontend/src/components/game/subPong.tsx
@@ -1,11 +1,16 @@
-import { OnDataOptions, useSubscription } from '@apollo/client'
 import React from 'react'
+import { ApolloError, OnDataOptions, useSubscription } from '@apollo/client'
 import { pongDataSubscription } from './graphql'
-import { PongGame } from '../../gql/graphql'
+import {
+  PongDataSubscription,
+  PongDataSubscriptionVariables,
+  PongGame
+} from '../../gql/graphql'
 
 type Props = {
   gameId: string
   updateGameElement: (pongData: PongGame) => void
+  setGameError: (error: ApolloError) => void
 }
 
 export const SubPong: React.FC<Props> = (props: Props) => {
@@ -14,13 +19,16 @@ export const SubPong: React.FC<Props> = (props: Props) => {
     props.updateGameElement(data)
   }
 
-  useSubscription(pongDataSubscription, {
-    variables: { gameId: props.gameId },
-    onData: listenPongData,
-    onError: (error) => {
-      console.log('SubMatchmaking: onError: error = ' + JSON.stringify(error))
+  useSubscription<PongDataSubscription, PongDataSubscriptionVariables>(
+    pongDataSubscription,
+    {
+      variables: { gameId: props.gameId },
+      onData: listenPongData,
+      onError: (error) => {
+        props.setGameError(error)
+      }
     }
-  })
+  )
 
   return null
 }
