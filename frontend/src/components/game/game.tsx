bec9a320881714eaec07e4b48e6e4b8ab9c2e91a
Isioblast <th.dv74@gmail.com>
2023-12-14 12:32:07 +0100

    Pong: add query to check if the game  id in the store is still valid

--- frontend/src/components/game/game.tsx
+++ frontend/src/components/game/game.tsx
@@ -3,40 +3,58 @@ import { Pong } from './Pong'
 import { Matchmaking } from '../matchmaking/matchmaking'
 import { useAppDispatch, useAppSelector } from '../../store/hooks'
 import { setGameIdValue } from '../../store/slices/gameId.slice'
+import { useQuery } from '@apollo/client'
+import {
+  IsGameValidQuery,
+  IsGameValidQueryVariables,
+  User
+} from '../../gql/graphql'
+import { isGameValid } from './graphql'
 
 // NOTE pour update les infos en fin de partie :
 // changer le lev en bdd
 // dispatch(setUserInformations())
 
 export const Game: React.FC = () => {
-  const username: string | undefined = useAppSelector(
-    (state) => state.userInformations.user?.username
-  )
-  const playerId: string | undefined = useAppSelector(
-    (state) => state.userInformations.user?.id
+  const user: User | null = useAppSelector(
+    (state) => state.userInformations.user
   )
   const dispatch = useAppDispatch()
   let gameId: string | null = useAppSelector(
     (state) => state.gameIdInformation.gameId
   )
-
   console.log('Game: gameId = ' + gameId)
+
+  const { data, loading, error } = useQuery<
+    IsGameValidQuery,
+    IsGameValidQueryVariables
+  >(isGameValid, {
+    skip: gameId === null || user === null,
+    variables: { gameId: gameId!, userId: user?.id! },
+    fetchPolicy: 'cache-and-network'
+  })
 
   function setGameId(id: string | null) {
     dispatch(setGameIdValue(id))
   }
 
-  if (username === undefined || playerId === undefined) {
+  if (user === null || user?.id === undefined) {
     return <p>Error : Username is not set.</p>
   }
-  if (gameId !== null) {
-    return <Pong gameId={gameId} username={username} playerId={playerId} />
+  if (gameId !== null && data && data.isGameValid === true) {
+    return <Pong gameId={gameId} username={user.username} playerId={user.id} />
+  }
+  if (loading) {
+    return <p>Loading...</p>
+  }
+  if (error) {
+    return <p>An error occured while connecting to the server</p>
   }
   return (
     <>
       <Matchmaking
-        username={username}
-        playerId={playerId}
+        username={user.username}
+        playerId={user.id}
         setGameId={setGameId}
       />
     </>
