03a73036e8ffdfa803dc6a2a8dcbb011fc9b6100
Isioblast <th.dv74@gmail.com>
2023-12-15 19:08:15 +0100

    Refactor the game matchamking

--- frontend/src/components/matchmaking/game-selection.tsx
+++ frontend/src/components/matchmaking/game-selection.tsx
@@ -1,31 +1,102 @@
 import React from 'react'
-import { EGameType } from '../../gql/graphql'
+import {
+  AddPlayerToMatchmakingQueueMutation,
+  AddPlayerToMatchmakingQueueMutationVariables,
+  EGameType,
+  IsUserInGameQueueQuery,
+  IsUserInGameQueueQueryVariables,
+  RemovePlayerFromMatchmakingQueueMutation,
+  RemovePlayerFromMatchmakingQueueMutationVariables
+} from '../../gql/graphql'
 import { GameInvitationPanel } from '../game-invitation-panel/game-invitations-panel'
+import { useMutation, useQuery } from '@apollo/client'
+import {
+  addPlayerToMatchmakingQueue,
+  isPlayerQueued,
+  removePlayerFromQueue
+} from './graphql'
 
 type Props = {
   username: string
   playerId: string
-  setGameType: React.Dispatch<React.SetStateAction<EGameType | null>>
 }
 
 export const GameSelection: React.FC<Props> = (props) => {
   console.log('GameSelection')
 
-  function gameClassic() {
-    console.log('gameClassic')
-    props.setGameType(EGameType.Classic)
+  const {
+    data: queryData,
+    loading: queryLoading,
+    error: queryError
+  } = useQuery<IsUserInGameQueueQuery, IsUserInGameQueueQueryVariables>(
+    isPlayerQueued,
+    { variables: { userId: props.playerId }, fetchPolicy: 'cache-and-network' }
+  )
+
+  const [
+    addPlayerToMatchmaking,
+    { loading: addPlayerLoading, error: addPlayerError }
+  ] = useMutation<
+    AddPlayerToMatchmakingQueueMutation,
+    AddPlayerToMatchmakingQueueMutationVariables
+  >(addPlayerToMatchmakingQueue)
+
+  function join(type: EGameType) {
+    addPlayerToMatchmaking({
+      variables: {
+        nickname: props.username,
+        playerId: props.playerId,
+        gameType: type
+      },
+      refetchQueries: [
+        {
+          query: isPlayerQueued,
+          variables: { userId: props.playerId },
+          fetchPolicy: 'network-only'
+        }
+      ]
+    })
   }
 
-  function gameExtra() {
-    console.log('gameSpecial')
-    props.setGameType(EGameType.Special)
-  }
+  const [
+    removeFromQueue,
+    { loading: leaveQueueLoading, error: leaveQueueError }
+  ] = useMutation<
+    RemovePlayerFromMatchmakingQueueMutation,
+    RemovePlayerFromMatchmakingQueueMutationVariables
+  >(removePlayerFromQueue)
 
+  if (queryLoading || leaveQueueLoading || addPlayerLoading) {
+    return <p>Loading...</p>
+  }
+  if (queryError || leaveQueueError || addPlayerError) {
+    return <p>Error while requesting the server</p>
+  }
+  if (queryData?.isUserInGameQueue === true) {
+    return (
+      <button
+        onClick={() => {
+          removeFromQueue({
+            variables: { playerId: props.playerId },
+            refetchQueries: [
+              {
+                query: isPlayerQueued,
+                variables: { userId: props.playerId },
+                fetchPolicy: 'network-only'
+              }
+            ]
+          })
+        }}
+      >
+        Leave Queue
+      </button>
+    )
+  }
   return (
     <div className='game-selection'>
       <h2>Hello {props.username}, select your game type :</h2>
-      <button onClick={gameClassic}>Classic Game</button>
-      <button onClick={gameExtra}>Extra Game</button>
+      <button onClick={() => join(EGameType.Classic)}>Classic Game</button>
+      <button onClick={() => join(EGameType.Special)}>Extra Game</button>
       <GameInvitationPanel playerId={props.playerId} />
     </div>
   )
--- frontend/src/components/matchmaking/matchmaking.tsx
+++ frontend/src/components/matchmaking/matchmaking.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import { WaitingGame } from './waiting-game'
+import { GameSelection } from './game-selection'
 
 type Props = {
   username: string
   playerId: string
-  setGameId: (id: string | null) => void
 }
 
 export const Matchmaking: React.FC<Props> = (props) => {
   return (
     <div id='matchmaking'>
       <h1>Matchmaking</h1>
-      <WaitingGame username={props.username} playerId={props.playerId} />
+      <GameSelection username={props.username} playerId={props.playerId} />
     </div>
   )
 }
