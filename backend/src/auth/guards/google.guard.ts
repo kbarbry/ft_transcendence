ea0e7c6a737da7aa11429043018044011f6828a8
Kellian Barbry <86127065+kbarbry@users.noreply.github.com>
2023-12-19 01:03:12 +0100

    Update google.guard.ts

--- backend/src/auth/guards/google.guard.ts
+++ backend/src/auth/guards/google.guard.ts
@@ -4,7 +4,6 @@ import { AuthGuard } from '@nestjs/passport'
 @Injectable()
 export class GoogleAuthGuard extends AuthGuard('google') {
   async canActivate(context: ExecutionContext) {
-    console.log('guard')
     const activate = (await super.canActivate(context)) as boolean
     const request = context.switchToHttp().getRequest()
     await super.logIn(request)
