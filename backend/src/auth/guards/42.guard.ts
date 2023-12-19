d156ab3f19e0f399216ed84bc1f90802c0aa338a
Robin <Geral.robin@yahoo.fr>
2023-12-19 17:28:37 +0100

    redirect when error42

--- backend/src/auth/guards/42.guard.ts
+++ backend/src/auth/guards/42.guard.ts
@@ -1,12 +1,24 @@
-import { Injectable, ExecutionContext } from '@nestjs/common'
+import {
+  Injectable,
+  ExecutionContext,
+  UnauthorizedException
+} from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
 export class School42AuthGuard extends AuthGuard('42') {
   async canActivate(context: ExecutionContext) {
-    const activate = (await super.canActivate(context)) as boolean
-    const request = context.switchToHttp().getRequest()
-    await super.logIn(request)
-    return activate
+    try {
+      const activate = (await super.canActivate(context)) as boolean
+      const request = context.switchToHttp().getRequest()
+      await super.logIn(request)
+      return activate
+    } catch (err) {
+      if (err instanceof UnauthorizedException) {
+        const response = context.switchToHttp().getResponse()
+        response.redirect('http://127.0.0.1:5173/')
+      }
+      throw err
+    }
   }
 }
