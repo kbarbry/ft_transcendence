0ef67e559a22120d456533a3c9ab3d54adc26889
Robin <Geral.robin@yahoo.fr>
2023-12-20 17:58:37 +0100

    layout 2fa

--- frontend/src/auth/2fa/settings.tsx
+++ frontend/src/auth/2fa/settings.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import { getToken } from './getToken'
-import QRCode from 'qrcode.react'
 import { useAppSelector } from '../../store/hooks'
 import { useLocation } from 'wouter'
 import { unset2fa } from './unset2fa'
 import { validateSecret } from './validateToken'
 import ErrorNotification from '../../notifications/ErrorNotificartion'
-import { Button } from 'antd'
+import { Button, Col, Divider, Row, Space, QRCode } from 'antd'
+import TransPicture from '/transcendence.png'
 
 export const Settings: React.FC = () => {
   const [otpCode, setOtpCode] = useState('')
@@ -16,6 +16,8 @@ export const Settings: React.FC = () => {
   const user = useAppSelector((state) => state.userInformations.user)
   const userId = user?.id
   if (!user) throw new Error()
+
+  console.log(user.validation2fa)
 
   const handleGetSecretClick = () => {
     getToken(userId)
@@ -62,36 +64,65 @@ export const Settings: React.FC = () => {
   }
 
   return (
-    <div>
-      <h1>Settings</h1>
-      <h2>2fa configuration</h2>
-      <Button onClick={handleGetSecretClick}>Get your own QR Code!</Button>
+    <Row gutter={[16, 16]} style={{ height: '100%', width: '100%' }}>
+      <Col span={24} style={{ height: '100%', overflowY: 'auto' }}>
+        <Space
+          direction='vertical'
+          style={{ width: '100%' }}
+          className='unselectable'
+        >
+          <h2
+            style={{
+              marginBottom: '0px',
+              textAlign: 'center'
+            }}
+          >
+            2FA Pannel
+          </h2>
+          <Divider
+            style={{ height: '10px', margin: '0px', marginTop: '10px' }}
+          />
+        </Space>
+        <Space
+          style={{
+            height: '70%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <Button onClick={handleGetSecretClick}>Get your own QR Code!</Button>
 
-      {otpAuthURL && (
-        <div>
-          <p>QR Code:</p>
-          <div style={{ border: '2px solid white', display: 'inline-block' }}>
-            <QRCode value={otpAuthURL} size={250} />
-          </div>
-        </div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='otpCode'>Enter OTP Code:</label>
-        <input
-          type='text'
-          id='otpCode'
-          name='otpCode'
-          value={otpCode}
-          onChange={(e) => setOtpCode(e.target.value)}
-        />
-        <Button onClick={handleValidateSecretClick} htmlType='submit'>
-          Validate
-        </Button>
-        <Button onClick={handleUnset2faClick} htmlType='submit'>
-          Disable 2fa
-        </Button>
-      </form>
-    </div>
+          {otpAuthURL && (
+            <div>
+              <p>QR Code:</p>
+              <div
+                style={{ border: '2px solid white', display: 'inline-block' }}
+              >
+                <QRCode value={otpAuthURL} size={250} icon={TransPicture} />
+              </div>
+            </div>
+          )}
+          <form onSubmit={handleSubmit}>
+            <label htmlFor='otpCode'>Enter OTP Code:</label>
+            <input
+              type='text'
+              id='otpCode'
+              name='otpCode'
+              value={otpCode}
+              onChange={(e) => setOtpCode(e.target.value)}
+            />
+            <Button onClick={handleValidateSecretClick} htmlType='submit'>
+              Validate
+            </Button>
+            <Button onClick={handleUnset2faClick} htmlType='submit'>
+              Disable 2fa
+            </Button>
+          </form>
+        </Space>
+      </Col>
+    </Row>
   )
 }
 
